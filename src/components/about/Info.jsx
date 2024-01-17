87ab1ab108e17e3c873f1f0809fe0aff5672fc70
Vini <126819626+vin1i@users.noreply.github.com>
2024-01-17 01:53:00 -0300

    Update Info.jsx

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -8,7 +8,7 @@ const Info = () => {
         <i class='bx bx-award about__icon'></i>
 
             <h3 className="about__title">Experiência</h3>
-            <span className="about__subtitle"> 1 ano estudando</span>
+            <span className="about__subtitle"> 1 ano </span>
         </div>
 
         <div className="about__box">
