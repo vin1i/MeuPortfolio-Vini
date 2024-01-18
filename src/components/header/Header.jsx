c27eeaee66c12f3f68779136bc2393eea1025b62
Vini <126819626+vin1i@users.noreply.github.com>
2024-01-18 12:31:12 -0300

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -20,28 +20,28 @@
 
                     <a href="#home" className="nav__link active-link">
                         <i className="uil uil-estate nav__icon"></i>
-                        Home
+                       Início
                     </a>
                 </li>
 
                 <li className="nav__item">
                     <a href="#about" className="nav__link">
                         <i className="uil uil-user nav__icon"></i>
-                        About
+                      Sobre
                     </a>
                 </li>
 
                 <li className="nav__item">
                     <a href="#skills" className="nav__link">
                         <i className="uil uil-file-alt nav__icon"></i>
-                        Skills
+                      Habilidades
                     </a>
                 </li>
 
                 <li className="nav__item">
                     <a href="#services" className="nav__link">
                         <i className="uil uil-briefcase-alt nav__icon"></i>
-                        Services
+                        Serviços
                     </a>
                 </li>
 
@@ -55,7 +55,7 @@
                 <li className="nav__item">
                     <a href="#contact" className="nav__link">
                         <i className="uil uil-message nav__icon"></i>
-                        Contact
+                        Contato
                     </a>
                 </li>
                 
